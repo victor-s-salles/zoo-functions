d9da9ff9a17cfdf59e390c4d0fab628848e81f9f
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-26 14:59:06 -0300

    Resolvido requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,5 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) { }
+function getSpeciesByIds(...ids) {
+  const animals = species.filter((elememento) => ids.some((testeID) => elememento.id === testeID));
+
+  return animals;
+}
 
 module.exports = getSpeciesByIds;
