d9ddd03b85d9225c2f5f48bd9a54805f031f0c82
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-26 16:55:09 -0300

    Resolvido requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const colaboradores = data.employees;
+  const colaborador = colaboradores.find((test) => test.id === id);
+  const testeGerente = colaboradores.some((test) => test.managers
+    .some((test2) => (test2 === colaborador.id)));
+  return testeGerente;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const colaboradores = data.employees;
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const pessoas = colaboradores.filter((test) => (test.managers
+    .some((test2) => test2 === managerId)));
+  const nomePessoas = pessoas.map((pGeren) => `${pGeren.firstName} ${pGeren.lastName}`);
+  return nomePessoas;
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+// console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
