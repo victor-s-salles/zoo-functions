86cda5273064912ad103f420e22a5217671f3ff6
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-26 15:42:32 -0300

    Resolvido requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const lista = data.species;
+  const animalBuscado = lista.filter((elemento) => elemento.name === animal);
+  // console.log(animalBuscado);
+  const [arrayResidents] = animalBuscado;
+  console.log(arrayResidents);
+  const { residents } = arrayResidents;
+  return residents.every((test) => test.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
+
+// console.log(getAnimalsOlderThan('otters', 7));
+// // console.log(getAnimalsOlderThan('penguins', 10));
+// // getAnimalsOlderThan('penguins', 10);
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const animals = species.filter((elememento) => ids.some((testeID) => elememento.id === testeID));
+  const animals = data.species.filter((elememento) => ids.some((testeID) =>
+    (elememento.id === testeID)));
 
   return animals;
 }
