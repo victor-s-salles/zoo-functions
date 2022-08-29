40e0b4f5bbbdef4367ad369eb5febaed09cc9272
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-29 14:35:58 -0300

    Resolvido o requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,19 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se caso não receber nenhum parametro, returna Undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Testa se caso não receber uma string, retornar mensagem de erro', () => {
+    expect(handlerElephants(123)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Testa se passado o name recebe o nome elephants', () => {
+    expect(handlerElephants('name')).toBe('elephants');
+  });
+  it('Testa se passanbdo cont retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Testa se passando averageAge retorna a media de idades dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+});
