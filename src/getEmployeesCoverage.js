d88296983da0d6c885258ff1abad38ace6d7de1a
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-29 18:55:59 -0300

    Resolvido o requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const collaborators = data.employees;
+const animalsList = data.species;
+
+const getSpecies = (id) => {
+  const collaborator = collaborators.find((element) => element.id === id);
+  const animals = collaborator.responsibleFor.map((element) => animalsList
+    .find((element2) => element2.id === element));
+  const names = animals.map((element) => element.name);
+  const locations = animals.map((element) => element.location);
+  return [names, locations];
+};
+
+getSpecies('c1f50212-35a6-4ecd-8223-f835538526c2');
+
+const listCollaborators = () => {
+  const newList = collaborators.map((element) => ({
+    id: element.id,
+    fullName: `${element.firstName} ${element.lastName}`,
+    species: getSpecies(element.id)[0],
+    locations: getSpecies(element.id)[1],
+  }));
+  return newList;
+};
+
+const testExistence = (par) => {
+  const listaPessoas = listCollaborators();
+  const pessoa = listaPessoas.find((element) => element.fullName.includes(par.name)
+    || element.id.includes(par.id));
+  if (!pessoa) {
+    throw new Error('Informações inválidas');
+  }
+  return pessoa;
+};
+
+function getEmployeesCoverage(par) {
+  if (!par) {
+    return listCollaborators();
+  }
+  if (par.id || par.name) {
+    return testExistence(par);
+  }
 }
 
 module.exports = getEmployeesCoverage;
+
+getEmployeesCoverage();
