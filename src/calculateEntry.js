865898b14cbb47243efdc512e0c3e33379543b6f
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-29 15:15:22 -0300

    Resolvido o requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((person) => person.age < 18);
+  const adult = entrants.filter((person) => person.age >= 18 && person.age < 50);
+  const senior = entrants.filter((person) => person.age >= 50);
+  return { child: child.length, adult: adult.length, senior: senior.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const separateAge = countEntrants(entrants);
+  const { child, adult, senior } = separateAge;
+  const { prices } = data;
+  return (child * prices.child + adult * prices.adult + senior * prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
