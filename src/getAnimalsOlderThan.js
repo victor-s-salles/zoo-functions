723b285f9a23764adff144cd52b2d34c8c8d297b
victor-s-salles <victor.salles@engenharia.ufjf.br>
2023-01-03 18:28:13 -0300

    Atualizando arquivos

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,16 +2,15 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const lista = data.species;
-  const animalBuscado = lista.filter((elemento) => elemento.name === animal);
-  // console.log(animalBuscado);
-  const [arrayResidents] = animalBuscado;
-  console.log(arrayResidents);
-  const { residents } = arrayResidents;
-  return residents.every((test) => test.age >= age);
+  const animalBuscado = lista.find((elemento) => elemento.name === animal);
+  const { residents } = animalBuscado;
+
+  console.log(residents);
+  return animalBuscado.residents.every((test) => test.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
 
-// console.log(getAnimalsOlderThan('otters', 7));
+console.log(getAnimalsOlderThan('otters', 7));
 // // console.log(getAnimalsOlderThan('penguins', 10));
 // // getAnimalsOlderThan('penguins', 10);
